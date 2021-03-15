34f6f4b0442eee51e05695edcec7c6ca13be4582
Hien Le <lhien11@gmail.com>
2021-03-14 17:59:14 -0700

    default navigation options & config

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 
 import MealsNavigator from './navigation/MealsNavigator';
+
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
 
 const fetchFonts = () => {
   return Font.loadAsync({
